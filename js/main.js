38a7daa6760b1d514c4c6125b8eaee6161fd6d30
Lisandro Francucci <lisandro.francucci@gmail.com>
2023-01-17 11:55:50 -0300

    Add media for Collabs: 2022 - 2018

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function contentLoaded() {
+
+/* For Collabs section only */
+let collabsSection;
+if(collabsSection = document.getElementById('collabs-section')) {
+  
+  const collabsLists = document.querySelectorAll('.collabs-list');
+  console.log(collabsLists);
+  console.log(collabsLists[0].childNodes);
+
+
+}
+
+
+}
