b41f4c85378d65f83eb541998321712b5a2fcfe8
Lisandro Francucci <lisandro.francucci@gmail.com>
2023-01-18 15:58:45 -0300

    Complete translation system; Add files and folders for english version

--- js/main.js
+++ js/main.js
@@ -29,18 +29,78 @@ translateElement();
 function changePageLanguage() {
 
   let currentLocation = location.href;
-  const locationEN = 'barbara-togander/en/';
-  const locationES = 'barbara-togander/';
+  let locationEN = 'barbara-togander/en/';
+  let locationES = 'barbara-togander/';
 
-  if(pageLanguage === 'es') 
+  // If current language is 'es', change to 'en' vesrion of the page
+  if(pageLanguage == 'es') 
   {
+    console.log("ES");
+    console.log(currentLocation);
+
+    switch(true) 
+    {
+      case (currentLocation.includes('biografia/')):
+        locationES = locationES + 'biografia/';
+        locationEN = locationEN + 'bio/';
+      break;
+      case (currentLocation.includes('proyectos/')):
+        locationES = locationES + 'proyectos/';
+        locationEN = locationEN + 'projects/';
+      break;
+      case (currentLocation.includes('colaboraciones/')):
+        locationES = locationES + 'colaboraciones/';
+        locationEN = locationEN + 'collabs/';
+      break;
+      case (currentLocation.includes('entrevistas/')):
+        locationES = locationES + 'entrevistas/';
+        locationEN = locationEN + 'interviews/';
+      break;
+      default: 
+        location.href;
+    }
     location.href = currentLocation.replace(locationES, locationEN);
-  } 
-  // If language is 'en' go to en/ folder
-  else if(pageLanguage === 'en') 
+  }
+  // If current language is 'en', change to 'es' vesrion of the page
+  if(pageLanguage == 'en') 
   {
+    console.log("EN");
+    console.log(currentLocation);
+
+    switch(true) 
+    {
+      case (currentLocation.includes('bio/')):
+        locationES = locationES + 'biografia/';
+        locationEN = locationEN + 'bio/';
+      break;
+      case (currentLocation.includes('projects/')):
+        locationES = locationES + 'proyectos/';
+        locationEN = locationEN + 'projects/';
+      break;
+      case (currentLocation.includes('collabs/')):
+        locationES = locationES + 'colaboraciones/';
+        locationEN = locationEN + 'collabs/';
+      break;
+      case (currentLocation.includes('interviews/')):
+        locationES = locationES + 'entrevistas/';
+        locationEN = locationEN + 'interviews/';
+      break;
+      default: 
+        location.href;
+    }
     location.href = currentLocation.replace(locationEN, locationES);
   }
+
+
+  // if(pageLanguage === 'es') 
+  // {
+  //   location.href = currentLocation.replace(locationES, locationEN);
+  // } 
+  // If language is 'en' go to en/ folder
+  // else if(pageLanguage === 'en') 
+  // {
+  //   location.href = currentLocation.replace(locationEN, locationES);
+  // }
 }
 
 langSelector.addEventListener('click', changePageLanguage);
