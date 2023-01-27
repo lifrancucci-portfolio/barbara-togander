a08555a9531e82480497cf3762822ba1b36c5809
Lisandro Francucci <lisandro.francucci@gmail.com>
2023-01-27 10:43:30 -0300

    Fix lang-change mechanics for nav-menu

--- js/main.js
+++ js/main.js
@@ -1,24 +1,28 @@
 function contentLoaded() {
 
+const mainContainer = document.querySelector('.page_content');
+
 /* MENU DISPLAY FOR MOBILE */
 // Please adjust and check
-// const mainTitle = document.getElementById('main-title');
-// const mainNav = document.getElementById('main-navigation');
+if( mainContainer.classList.contains('home') ) 
+{
+  const mainTitle = document.getElementById('main-title');
+  const mainNav = document.getElementById('main-navigation');
 
-// mainTitle.addEventListener('click', function() {
-//   if(mainNav.classList.contains('active')) {
-//     mainNav.classList.remove('active');
-//   }
-//   else
-//   {
-//     mainNav.classList.add('active');
-//   }
-// })
+  mainTitle.addEventListener('click', function() {
+    if(mainNav.classList.contains('active')) {
+      mainNav.classList.remove('active');
+    }
+    else
+    {
+      mainNav.classList.add('active');
+    }
+  })
+}
  
 /* CHANGE LANGUAGE ON CLICK */ 
 const langSelector = document.getElementById('lang-selector');
 const translatableElements = document.querySelectorAll('.translatable-element');
-const mainContainer = document.querySelector('.page_content');
 let pageLanguage = mainContainer.lang;
 
 // Check for page language and display corresponding menu items:
@@ -35,72 +39,90 @@ function translateElement() {
     {
       item.style.display = 'block';
     }
-
   });
 }
 translateElement();
 
+
 // Navigate to corresponding page depending on the language set on main-container
 function changePageLanguage() {
 
-  let currentLocation = location.href;
-  let locationEN = 'barbara-togander/en/';
-  let locationES = 'barbara-togander/';
-
-  // If current language is 'es', change to 'en' vesrion of the page
-  if(pageLanguage == 'es') 
+  // If on homepage, just change items' lang (not page)
+  if( mainContainer.classList.contains('home') ) 
   {
-    console.log("ES");
-    console.log(currentLocation);
-
-    switch(true) 
+    switch( pageLanguage ) 
     {
-      case (currentLocation.includes('bio/')):
-        locationES = locationES + 'bio/';
-        locationEN = locationEN + 'bio/';
-      break;
-      case (currentLocation.includes('proyectos/')):
-        locationES = locationES + 'proyectos/';
-        locationEN = locationEN + 'projects/';
-      break;
-      case (currentLocation.includes('colaboraciones/')):
-        locationES = locationES + 'colaboraciones/';
-        locationEN = locationEN + 'collabs/';
-      break;
-      case (currentLocation.includes('entrevistas/')):
-        locationES = locationES + 'entrevistas/';
-        locationEN = locationEN + 'interviews/';
-      break;
-      default: 
-        location.href;
+      case 'en':
+        pageLanguage = 'es';
+        break;
+      case 'es':
+        pageLanguage = 'en';
+        break;
+      default:
+        pageLanguage = 'es';
     }
-    location.href = currentLocation.replace(locationES, locationEN);
+    translateElement(pageLanguage);
   }
-  // If current language is 'en', change to 'es' vesrion of the page
-  if(pageLanguage == 'en') 
+  // If not on homepage, change to corresponding lang site
+  else 
   {
-    switch(true) 
+    // Check for current location
+    let currentLocation = location.href;
+    const locationEN = 'barbara-togander/en/';
+    const locationES = 'barbara-togander/';
+
+    // If current language is 'es', change to 'en' vesrion of the page
+    if(pageLanguage == 'es') 
     {
-      case (currentLocation.includes('bio/')):
-        locationES = locationES + 'bio/';
-        locationEN = locationEN + 'bio/';
-      break;
-      case (currentLocation.includes('projects/')):
-        locationES = locationES + 'proyectos/';
-        locationEN = locationEN + 'projects/';
-      break;
-      case (currentLocation.includes('collabs/')):
-        locationES = locationES + 'colaboraciones/';
-        locationEN = locationEN + 'collabs/';
-      break;
-      case (currentLocation.includes('interviews/')):
-        locationES = locationES + 'entrevistas/';
-        locationEN = locationEN + 'interviews/';
-      break;
-      default: 
-        location.href;
+      switch(true) 
+      {
+        case (currentLocation.includes('bio/')):
+          locationES = locationES + 'bio/';
+          locationEN = locationEN + 'bio/';
+        break;
+        case (currentLocation.includes('proyectos/')):
+          locationES = locationES + 'proyectos/';
+          locationEN = locationEN + 'projects/';
+        break;
+        case (currentLocation.includes('colaboraciones/')):
+          locationES = locationES + 'colaboraciones/';
+          locationEN = locationEN + 'collabs/';
+        break;
+        case (currentLocation.includes('entrevistas/')):
+          locationES = locationES + 'entrevistas/';
+          locationEN = locationEN + 'interviews/';
+        break;
+        default: 
+          location.href;
+      }
+      location.href = currentLocation.replace(locationES, locationEN);
     }
-    location.href = currentLocation.replace(locationEN, locationES);
+    // If current language is 'en', change to 'es' vesrion of the page
+    if(pageLanguage == 'en') 
+    {
+      switch(true) 
+      {
+        case (currentLocation.includes('bio/')):
+          locationES = locationES + 'bio/';
+          locationEN = locationEN + 'bio/';
+        break;
+        case (currentLocation.includes('projects/')):
+          locationES = locationES + 'proyectos/';
+          locationEN = locationEN + 'projects/';
+        break;
+        case (currentLocation.includes('collabs/')):
+          locationES = locationES + 'colaboraciones/';
+          locationEN = locationEN + 'collabs/';
+        break;
+        case (currentLocation.includes('interviews/')):
+          locationES = locationES + 'entrevistas/';
+          locationEN = locationEN + 'interviews/';
+        break;
+        default: 
+          location.href;
+      }
+      location.href = currentLocation.replace(locationEN, locationES);
+    }
   }
 }
 
