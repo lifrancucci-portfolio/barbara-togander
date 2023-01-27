f5873e281f173a2d59f2501e9865ff23bc8f9ebf
Lisandro Francucci <lisandro.francucci@gmail.com>
2023-01-27 12:23:06 -0300

    Complete lang-change mechanics for translating page-slugs; Add translations

--- js/main.js
+++ js/main.js
@@ -43,7 +43,6 @@ function translateElement() {
 }
 translateElement();
 
-
 // Navigate to corresponding page depending on the language set on main-container
 function changePageLanguage() {
 
@@ -68,61 +67,116 @@ function changePageLanguage() {
   {
     // Check for current location
     let currentLocation = location.href;
-    const locationEN = 'barbara-togander/en/';
-    const locationES = 'barbara-togander/';
+    let newLocation;
+
+    // Create an array with page sections 
+    const pageSections = 
+    [
+      { esTitle: 'proyectos/',       enTitle: 'en/projects/' },
+      { esTitle: 'colaboraciones/',  enTitle: 'en/collabs/' },
+      { esTitle: 'entrevistas/',     enTitle: 'en/interviews/' },
+      { esTitle: 'bio/',             enTitle: 'en/bio/' },
+    ];
+
+    // Create an array of subpages if they have different names (ENGLISH / SPANISH )
+    const subpages = 
+    [
+      { 
+        esTitle: 'proy-audiovisual/',
+        enTitle: 'av-project/' 
+      },
+      { 
+        esTitle: 'residencia-los-dos-ombues/',       enTitle: 'los-dos-ombues-residency/' 
+      },
+      { 
+        esTitle: 'paraiso-en-blanco/',       enTitle: 'an-unsolved-blank-paradise/' 
+      },
+    ]
 
     // If current language is 'es', change to 'en' vesrion of the page
     if(pageLanguage == 'es') 
     {
-      switch(true) 
-      {
-        case (currentLocation.includes('bio/')):
-          locationES = locationES + 'bio/';
-          locationEN = locationEN + 'bio/';
-        break;
-        case (currentLocation.includes('proyectos/')):
-          locationES = locationES + 'proyectos/';
-          locationEN = locationEN + 'projects/';
-        break;
-        case (currentLocation.includes('colaboraciones/')):
-          locationES = locationES + 'colaboraciones/';
-          locationEN = locationEN + 'collabs/';
-        break;
-        case (currentLocation.includes('entrevistas/')):
-          locationES = locationES + 'entrevistas/';
-          locationEN = locationEN + 'interviews/';
-        break;
-        default: 
-          location.href;
-      }
-      location.href = currentLocation.replace(locationES, locationEN);
+      // First, change the Section
+      pageSections.forEach(section => {
+        if ( currentLocation.includes(section.esTitle) )
+        {
+          newLocation = currentLocation.replace(section.esTitle, section.enTitle);
+        }
+      })
+      // Then, if on a subpage, change the subpage
+      subpages.forEach(subpage => {
+        if( newLocation.includes(subpage.esTitle) )
+        {
+          newLocation = newLocation.replace(subpage.esTitle, subpage.enTitle);
+        }
+      })
+
+      // {
+      //   case (currentLocation.includes('bio/')):
+      //     locationES = locationES + 'bio/';
+      //     locationEN = locationEN + 'bio/';
+      //   break;
+      //   case (currentLocation.includes('proyectos/')):
+      //     locationES = locationES + 'proyectos/';
+      //     locationEN = locationEN + 'projects/';
+      //   break;
+      //   case (currentLocation.includes('colaboraciones/')):
+      //     locationES = locationES + 'colaboraciones/';
+      //     locationEN = locationEN + 'collabs/';
+      //   break;
+      //   case (currentLocation.includes('entrevistas/')):
+      //     locationES = locationES + 'entrevistas/';
+      //     locationEN = locationEN + 'interviews/';
+      //   break;
+      //   default: 
+      //     location.href;
+      // }
+      // location.href = currentLocation.replace(locationES, locationEN);
     }
     // If current language is 'en', change to 'es' vesrion of the page
     if(pageLanguage == 'en') 
     {
-      switch(true) 
-      {
-        case (currentLocation.includes('bio/')):
-          locationES = locationES + 'bio/';
-          locationEN = locationEN + 'bio/';
-        break;
-        case (currentLocation.includes('projects/')):
-          locationES = locationES + 'proyectos/';
-          locationEN = locationEN + 'projects/';
-        break;
-        case (currentLocation.includes('collabs/')):
-          locationES = locationES + 'colaboraciones/';
-          locationEN = locationEN + 'collabs/';
-        break;
-        case (currentLocation.includes('interviews/')):
-          locationES = locationES + 'entrevistas/';
-          locationEN = locationEN + 'interviews/';
-        break;
-        default: 
-          location.href;
-      }
-      location.href = currentLocation.replace(locationEN, locationES);
+      // First, change the Section
+      pageSections.forEach(section => {
+        if ( currentLocation.includes(section.enTitle) )
+        {
+          newLocation = currentLocation.replace(section.enTitle, section.esTitle);
+        }
+      })
+      // Then, if on a subpage, change the subpage
+      subpages.forEach(subpage => {
+        if( newLocation.includes(subpage.enTitle) )
+        {
+          newLocation = newLocation.replace(subpage.enTitle, subpage.esTitle);
+        }
+      })
+
+      // {
+      //   case (currentLocation.includes('bio/')):
+      //     locationES = locationES + 'bio/';
+      //     locationEN = locationEN + 'bio/';
+      //   break;
+      //   case (currentLocation.includes('projects/')):
+      //     locationES = locationES + 'proyectos/';
+      //     locationEN = locationEN + 'projects/';
+      //   break;
+      //   case (currentLocation.includes('collabs/')):
+      //     locationES = locationES + 'colaboraciones/';
+      //     locationEN = locationEN + 'collabs/';
+      //   break;
+      //   case (currentLocation.includes('interviews/')):
+      //     locationES = locationES + 'entrevistas/';
+      //     locationEN = locationEN + 'interviews/';
+      //   break;
+      //   default: 
+      //     location.href;
+      // }
+      // location.href = currentLocation.replace(locationEN, locationES);
     }
+
+    // Finally, got to location
+    location.href = newLocation;
+
   }
 }
 
